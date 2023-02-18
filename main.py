566b22c3deaf6141eecad2f2b26996230e76fae3
Gabe Hoban <gabehoban1@posteo.de>
2023-02-18 13:11:27 -0500

    Ignore system IP addresses.
    Signed-off-by: Gabe Hoban <gabehoban1@posteo.de>

--- main.py
+++ main.py
@@ -28,33 +28,33 @@ def fixSchema():
   result.fetchall()
 
 def main():
-  emptyAddresses = []
+  ignoreAddresses = os.environ.get("GT_IGNORE_IPS").split(",")
 
   # Verify schema is correct
   if not baseDB.query(f"SHOW COLUMNS from %s LIKE 'latitude'" %(baseTable)).fetchall():
     fixSchema()
-  pass
 
   for session in baseDB.fetch(f"SELECT id, %s FROM %s WHERE longitude IS NULL" %(baseColumn, baseTable)):
-    locationQuery = f"""
-        SELECT latitude, longitude, postal_code
-        FROM geoip2_network
-        WHERE inet6_aton("%s") between network_start and network_end
-        limit 1;
-    """ %(session[1])
-    result=geoDB.query(locationQuery).fetchall()
-    if result:
-      updateQuery = f"""
-          UPDATE %s
-          SET latitude=%f, longitude=%f, postal_code="%s"
-          WHERE id="%s"
-      """ %(baseTable, result[0][0], result[0][1], result[0][2], session[0])
-      updateResult = baseDB.update(updateQuery)
-      
-      if updateResult:
-        print("Session ID: %s, successfully updated location data." %(session[0]))
-    else:
-      print("[WARN] Unable to tag %s (%s)." %(session[0], session[1]))
+    if session[1] not in ignoreAddresses:
+      locationQuery = f"""
+          SELECT latitude, longitude, postal_code
+          FROM geoip2_network
+          WHERE inet6_aton("%s") between network_start and network_end
+          limit 1;
+      """ %(session[1])
+      result=geoDB.query(locationQuery).fetchall()
+      if result:
+        updateQuery = f"""
+            UPDATE %s
+            SET latitude=%f, longitude=%f, postal_code="%s"
+            WHERE id="%s"
+        """ %(baseTable, result[0][0], result[0][1], result[0][2], session[0])
+        updateResult = baseDB.update(updateQuery)
+
+        if updateResult:
+          print("Session ID: %s, successfully updated location data." %(session[0]))
+      else:
+        print("[WARN] Unable to tag %s (%s)." %(session[0], session[1]))
 
 loop = task.LoopingCall(main)
 loop.start(timeout)
