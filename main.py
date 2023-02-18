5bfe754535a69426b2085fa1d96df2823169b700
Gabe Hoban <gabehoban1@posteo.de>
2023-02-18 12:55:37 -0500

    Fix ips with unknown locations.
    Signed-off-by: Gabe Hoban <gabehoban1@posteo.de>

--- main.py
+++ main.py
@@ -42,16 +42,19 @@ def main():
         WHERE inet6_aton("%s") between network_start and network_end
         limit 1;
     """ %(session[1])
-    result=geoDB.query(locationQuery).fetchall()[0]
-    updateQuery = f"""
-        UPDATE %s
-        SET latitude=%f, longitude=%f, postal_code="%s"
-        WHERE id="%s"
-    """ %(baseTable, result[0], result[1], result[2], session[0])
-    updateResult = baseDB.update(updateQuery)
-    
-    if updateResult:
-      print("Session ID: %s, successfully updated location data." %(session[0]))
+    result=geoDB.query(locationQuery).fetchall()
+    if result:
+      updateQuery = f"""
+          UPDATE %s
+          SET latitude=%f, longitude=%f, postal_code="%s"
+          WHERE id="%s"
+      """ %(baseTable, result[0][0], result[0][1], result[0][2], session[0])
+      updateResult = baseDB.update(updateQuery)
+      
+      if updateResult:
+        print("Session ID: %s, successfully updated location data." %(session[0]))
+    else:
+      print("[WARN] Unable to tag %s (%s)." %(session[0], session[1]))
 
 loop = task.LoopingCall(main)
 loop.start(timeout)
